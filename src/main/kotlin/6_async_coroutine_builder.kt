4c01b620b96e06db7567a6dbdf1a200a5e8f1130
Shadab <shadabjhu00447@gmail.com>
2023-08-04 15:04:46 +0530

    async_coroutine_builder

---
+++ src/main/kotlin/6_async_coroutine_builder.kt
@@ -0,0 +1,25 @@
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
+
+fun main() = runBlocking {
+
+    println("Main program starts: ${Thread.currentThread().name}")  // main thread
+
+    val defferedJob: Deferred<Int> = async {
+        println("Fake work starts: ${Thread.currentThread().name}")     // Thread: main
+        delay(1000)   // Coroutine is suspended but Thread: main is free (not blocked)
+        println("Fake work finished: ${Thread.currentThread().name}") // Thread: main
+        15
+
+    }
+
+    val num: Int = defferedJob.await()
+    println("Main program ends: ${Thread.currentThread().name}")    // main thread
+
+
+
+
+}

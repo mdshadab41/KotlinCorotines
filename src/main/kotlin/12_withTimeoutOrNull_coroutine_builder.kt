5c40473b5d4897b51c199e924fff6fad7a814277
Shadab <shadabjhu00447@gmail.com>
2023-08-04 23:23:16 +0530

    withTimeoutOrNull

---
+++ src/main/kotlin/12_withTimeoutOrNull_coroutine_builder.kt
@@ -0,0 +1,21 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeoutOrNull
+
+fun main() = runBlocking {    // Creates a blocking coroutine that executes in current thread (main)
+
+    println("Main program starts: ${Thread.currentThread().name}")  // main thread
+
+    val result: String? = withTimeoutOrNull(2000) {
+        for (i in 0..500) {
+            print("$i.")
+            delay(500)
+        }
+
+        "I am done"
+    }
+
+    print("Result: $result")
+
+    println("\nMain program ends: ${Thread.currentThread().name}")    // main thread
+}

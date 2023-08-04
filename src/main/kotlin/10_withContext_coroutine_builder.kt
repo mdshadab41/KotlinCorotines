a3434bf64e0192754ebb05cc1570d51cea0d25b1
Shadab <shadabjhu00447@gmail.com>
2023-08-04 23:18:07 +0530

    withcontext1

---
+++ src/main/kotlin/10_withContext_coroutine_builder.kt
@@ -0,0 +1,30 @@
+import kotlinx.coroutines.*
+
+
+fun main() = runBlocking {
+
+    println("Main program starts: ${Thread.currentThread().name}")  // main thread
+    val job: Job = launch(Dispatchers.Default) {     // Thread T1: Creates a non-blocking coroutine
+        try {
+            for (i in 0..500) {
+                print("$i.")
+                delay(5)    // or use yield() or any other suspending function as per your need.
+            }
+        } catch (ex: CancellationException) {
+            print("\nException caught safely: ${ex.message}")
+        } finally {
+            withContext(NonCancellable) {
+                delay(1000)     // Generally we don't use suspending function in finally
+                print("\nClose resources in finally")
+            }
+        }
+    }
+
+    delay(10)  // Let's print a few values before we cancel
+    job.cancel(CancellationException("My own crash message"))
+    job.join()
+
+    println("\nMain program ends: ${Thread.currentThread().name}")    // main thread
+}
+
+
